8f526ca419aa49f390a0977d4c246f7e67b50368
Sai Krishna Koruprolu <65915645+SaiKrishna-Koruprolu@users.noreply.github.com>
2023-09-13 17:26:44 -0500

    committing the changes:front UI

--- PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/ViewController.swift
+++ PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/ViewController.swift
@@ -8,34 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        class YourViewController: ViewController {
-
-         
-
-            @IBOutlet weak var headerLBL: UILabel!
-
-         
-
-            override func viewDidLoad() {
-                super.viewDidLoad()
-
-         
-
-                let labelText = "స్వాగతం\nPlay with AlphaNumerics™"
-                headerLBL.text = labelText
-            }
-
-         
-
+        self.headerLBL.text = String(format: "%@\nPlay with AlphaNumerics™️", "\u{0c38}\u{0c4d}\u{0c35}\u{0c3e}\u{0c17}\u{0c24}\u{0c02}")
+            
+        //self.numbersStepper[0].isEnabled=false
+        //self.numbersStepper[1].isEnabled=false
+        
+        
+        
+        
+            
+            
             
         }
+    
+    
+    @IBOutlet weak var headerLBL: UILabel!
+    
+    
+    @IBOutlet weak var secondNumLBL: UILabel!
+    
+    @IBOutlet weak var firstNumLBL: UILabel!
+    
+    @IBAction func numberSWCH(_ sender: UISwitch) {
     }
+    
+    @IBAction func stringSWCH(_ sender: UISwitch) {
+    }
+    
+    @IBOutlet var optionSWCH: [UISwitch]!
+    
+    @IBAction func firstStrTF(_ sender: UITextField) {
+    }
+    
+    @IBAction func secondStrTF(_ sender: UITextField) {
+    }
+    
+    
+    @IBAction func firstNumStepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func secondNumStepper(_ sender: UIStepper) {
+    }
+    
+    @IBOutlet var numbersStepper: [UIStepper]!
+    
+    
+    @IBOutlet weak var messageTV: UITextView!
+    
+    @IBAction func generatePattern(_ sender: UIButton) {
+    }
+    
 
-
+    @IBAction func manipulateStrings(_ sender: UIButton) {
+    }
+    
+    
+    @IBOutlet var stringAndPatternBTN: [UIButton]!
+ 
+    
+    @IBAction func onReset(_ sender: UIButton) {
+    }
+    
 }
+
 
